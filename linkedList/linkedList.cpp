c8f382b68277cca08dc7afbf267267c9e877fbcb
IAMHUDA <hudamiftahul362@gmail.com>
2023-05-18 16:47:12 +0700

    menggunakan perulangan dan pada searchnode dan current sebagai pointer

--- linkedList/linkedList.cpp
+++ linkedList/linkedList.cpp
@@ -55,7 +55,7 @@ bool searchNode(int nim, Node* current, Node* previous)
 	previous = START; 
 	current = START;
 	while (current != NULL && nim > current->noMhs)
-	{
+	{ 
 		previous = current;
 		current = current->next;
 	}
